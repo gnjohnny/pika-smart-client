c87f3eeeed8ea6bc4243daba7313bb3bcb9b8259
John <54johnmbugua@gmail.com>
2026-01-18 23:31:53 +0300

    Fix a bug in global file types

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,10 +1,15 @@
+import type { Control, FieldPath, FieldValues } from 'react-hook-form'
+
 export {};
 
 declare global{
-     type FormFieldInputProps = {
-        control: any;
-        name: string;
+     type FormFieldInputProps<
+        TFieldValues extends FieldValues = FieldValues,
+        TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+    > = {
+        control: Control<TFieldValues>;
+        name: TName;
         placeholder: string;
         text: string;
-    };
+     };
 }
